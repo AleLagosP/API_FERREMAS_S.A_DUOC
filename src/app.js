fcdfb661b2640c6c22f212ce16329b7e38ba9979
Fenixssj <ale.lagosp@duocuc.cl>
2025-05-12 13:22:06 -0400

    HTML EN RESOLVER

--- src/app.js
+++ src/app.js
@@ -1,23 +1,25 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');  // Asegúrate de importar 'path'
 
 const app = express();
-const port = process.env.port || 5500;
+const port = process.env.PORT || 5500;
 
 // configuración body parser para permitir json, y url encoded
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const connection = require('./config/config.js');
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Conexión a la base de datos - APIs:
-const clientes = require('./controllers/clientes');
-const productos = require('./controllers/productos');
-const ventas = require('./controllers/ventas');
+const clientesRouter = require('../controllers/clientes');
+const productosRouter = require('../controllers/productos');
+const ventasRouter = require('../controllers/ventas');
 
 // Configurar cabeceras y cors
-app.use((req, res, next) => {
+app.use((_req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
@@ -28,19 +30,18 @@ app.use((req, res, next) => {
 // Función controladores
 function controladores() {
     // Rutas principales de la API
-    app.get("/api/test/", function(request, response) {
+    app.get("/api/test/", function(_request, response) {
         response.send("Bienvenido a API REST de ferremas");
     });
 
-    // Usar el controlador 'buscar_todo' en la ruta'
-    app.get('/api/clientes/todos', clientes.buscar_todo);
-    app.use('/api/productos', productos);
-    app.use('/api/ventas', ventas);
+    // Usar el controlador 'buscar_todo' en la ruta
+    app.use('/api/clientes', clientesRouter);
+    app.use('/api/productos', productosRouter);
+    app.use('/api/ventas', ventasRouter);
 }
 
-// Se inicia servidor
+// Iniciar servidor
 app.listen(port, function () {
-    console.log(`Servidor esta corriendo! http://localhost:${port}/api/test/`);
+    console.log(`Servidor está corriendo en http://localhost:${port}/api/test/`);
     controladores(); // Llamamos a la función controladores para definir rutas adicionales
 });
-
